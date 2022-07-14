c14c5a3e6d3cc6e258eccb6c9860b34d95230abb
Cem <cemkgrbz@gmail.com>
2022-07-14 12:01:23 +0200

    modified solutions

--- solution.js
+++ solution.js
@@ -1,18 +1,29 @@
 //1
 
-function exponentialValues (x, y) {
-    for (let i = 1; i <= y; i++ )
-    console.log(x ** i)
+function exponentialValue(x, y) {
+    if (typeof x !== "number" || typeof y !== "number") {
+        console.log("x or y is not a number");
+      }
+    let exponentResult = 1;
+    let values = "";
+    for (let i = 0; i < y; i++) {
+        exponentResult *= x;
+        values += `${exponentResult} `;
+      }
+      console.log(values);
 };
-exponentialValues(5,6);
+
+exponentialValue(5,6);
 
 //2
 
-const fruit = 'banana';
+let fruit = 'banana';
 function printFavoriteFruit () {
+    fruit = 'watermelon'
     console.log('My favorite fruit is', fruit)
 }
 printFavoriteFruit (fruit)
+
 
 //
 
@@ -20,7 +31,18 @@ function exponent (a, b) {
     const result = a ** b;
     console.log(result);
 };
+// console.log(result);
 exponent(3, 5);
+
+// const exponent = (num1, num2) => {
+//     for (let i = 1; i <= num2; i++) {
+//       let result = Math.pow(num1, i);
+//       // console.log(`${num1} ^ ${i} is ${result}`);
+//     }
+//   };
+//   console.log(result);
+  
+//   exponent(2, 8);
 
 // console.log(result)
 // It's not possible to access the variable 'result'. It gives an error, called 'ReferenceError: result is not defined'. Because the variable 'result' is defined in the scope of the exponent function.
